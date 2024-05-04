4fbd0f537263a548c14290189a2f770b710e53a8
Felipe Villani <felipevillani.2000@hotmail.com>
2024-05-03 21:23:39 -0300

    feat: add icon

--- src/app/components/TripCard.jsx
+++ src/app/components/TripCard.jsx
@@ -1,3 +1,5 @@
+import PlaceRoundedIcon from '@mui/icons-material/PlaceRounded';
+
 function TripCard(props) {
 
   const formatOptions = { style: "currency", currency: "BRL" };
@@ -6,7 +8,10 @@ function TripCard(props) {
   return (
     <div className="trip-card">
       <img src={props.imgUrl}></img>
-      <h2>{props.destination}</h2>
+      <div className='destination'>
+        <PlaceRoundedIcon style={{color: "red"}} />
+        <h2>{props.destination}</h2>
+      </div>     
       <p>{currencyFormatter.format(props.cost)}</p>
     </div>
   );
