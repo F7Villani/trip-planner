425458a49174db650af874f2aa5c63fc251328db
Felipe Villani <felipevillani.2000@hotmail.com>
2024-05-05 15:32:43 -0300

    feat: implementando AddTripCard

--- src/app/components/AddTripCard/AddTripCard.jsx
+++ src/app/components/AddTripCard/AddTripCard.jsx
@@ -1,7 +1,12 @@
+import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
+import './AddTripCard.css';
+
 function AddTripCard(props){
-    return(
-        <>
-        </>
+    return(         
+        <div onClick={props.onClick} className="card add-card">
+            <p>Organize sua próxima viagem</p>
+            <AddCircleRoundedIcon className='add-icon'/>        
+        </div>
     );
 }
 
