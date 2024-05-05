a6b441c7c256f55bf6e038f5a91d6bc60c3b8417
Felipe Villani <felipevillani.2000@hotmail.com>
2024-05-05 15:31:15 -0300

    feat: separando componente Header

---
+++ src/app/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+function Header() {
+  return (
+    <header>
+      <img className="logo" src="./trip-planner-logo.png"></img>
+      <h1>Trip Planner</h1>
+    </header>
+  );
+}
+
+export default Header;
