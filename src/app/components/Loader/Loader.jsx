80106d75574bf117d44aee99ad915753b003fd94
Felipe Villani <felipevillani.2000@hotmail.com>
2024-05-06 17:02:25 -0300

    feat: simplificando loader

--- src/app/components/Loader/Loader.jsx
+++ src/app/components/Loader/Loader.jsx
@@ -3,12 +3,7 @@ import './Loader.css';
 function Loader(){
   return (
     <div className="loader">
-      <div className="plane">
-        <img src="https://zupimages.net/up/19/34/4820.gif" className="plane-img"></img>
-      </div>
-      <div className="earth-wrapper">
-        <div className="earth"></div>
-      </div>  
+      <div className="earth"></div>
     </div>
   );
 }
