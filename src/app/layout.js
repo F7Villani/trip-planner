b1c6ec4f2eff4dd1570808d6d133d2ae6d5466f4
Felipe Villani <felipevillani.2000@hotmail.com>
2024-05-05 22:50:04 -0300

    feat: changing favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className}>
+      <head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
       <body>
         <Header />
         <main>{children}</main>
