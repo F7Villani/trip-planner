4d819d1aa9ca68466dce99fd0b520e1e086d0a51
Felipe Villani <felipevillani.2000@hotmail.com>
2024-05-09 20:56:16 -0300

    feat: add grid

--- src/app/create-trip/page.tsx
+++ src/app/create-trip/page.tsx
@@ -5,34 +5,47 @@ import './CreateTrip.css';
 import GeoNamesService from "../services/GeoNamesService";
 import AutocompleteTextField from "../components/AutocompleteTextField/AutocompleteTextField";
 import Place from "../models/Place";
+import PexelsService from "../services/PexelsService";
+import PlaceCard from "../components/PlaceCard/PlaceCard";
+import Loader from "../components/Loader/Loader";
 
 function CreateTripPage() {
 
-  const [place, setPlace]  = useState(new Place());
+  const [place, setPlace] = useState(new Place());
 
   const placeService = new GeoNamesService();
+  const photoService = new PexelsService();
 
-  async function getPlaces(text: string){
+  async function getPlaces(text: string) {
     return await placeService.getPlacesByText(text);
   }
 
-  function onSelectPlace(place: Place){
+  async function onSelectPlace(place: Place) {
+    const query = `${place.city} city view ${place.country}`;
+    const urlPhoto = await photoService.getUrlPhotoByText(query);
+    place.urlPhoto = urlPhoto;
     setPlace(place);
   }
 
-  function placeToString(place: Place){
+  function placeToString(place: Place) {
     return place.city + ', ' + place.country;
   }
 
   return (
     <>
-      <h1>Hello World!</h1>
-      <AutocompleteTextField 
-        delay={500}
-        itemToString={placeToString} 
-        handleItemClick={onSelectPlace}
-        getItems={getPlaces} />
-      <h1>{place.city}</h1>
+      <h2>Escolha seu destino</h2>
+
+      <div className="create-trip-grid">
+        <div className="textfield-wrapper">
+          <AutocompleteTextField
+            delay={500}
+            itemToString={placeToString}
+            handleItemClick={onSelectPlace}
+            getItems={getPlaces} />
+        </div>
+        <p></p>
+        {place.isEmpty() ? null : <PlaceCard place={place}/>}
+      </div>      
     </>
   );
 }
