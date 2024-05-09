45065dd4999eab5cc68fbd0e55265163e88dbeaa
Felipe Villani <felipevillani.2000@hotmail.com>
2024-05-08 21:28:06 -0300

    feat: implementando creatTripPage

--- src/app/create-trip/page.jsx
+++ src/app/create-trip/page.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+"use client"
 
-function CreateTripPage(){
-    return <h1>Hello Wolrd!</h1>
+import React, { useState } from "react";
+import './CreateTrip.css';
+import GeoNamesService from "../services/GeoNamesService";
+import AutocompleteTextField from "../components/AutocompleteTextField/AutocompleteTextField";
+import Place from "../models/Place";
+
+function CreateTripPage() {
+
+  const [place, setPlace]  = useState({});
+
+  const placeService = new GeoNamesService();
+
+  async function getPlaces(text){
+    return await placeService.getPlacesByText(text);
+  }
+
+  function onSelectPlace(item){
+    setPlace(item);
+  }
+
+  function placeToString(place){
+    return place.city + ', ' + place.country;
+  }
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <AutocompleteTextField 
+        delay={500}
+        itemToString={placeToString} 
+        handleItemClick={onSelectPlace}
+        getItems={getPlaces} />
+      <h1>{place.city}</h1>
+    </>
+  );
 }
 
 export default CreateTripPage;
