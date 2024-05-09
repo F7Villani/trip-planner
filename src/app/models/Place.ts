f405969a14b55e76998b66baa6e231195b3e88b1
Felipe Villani <felipevillani.2000@hotmail.com>
2024-05-09 20:54:39 -0300

    feat: add isEmpty

--- src/app/models/Place.ts
+++ src/app/models/Place.ts
@@ -2,14 +2,22 @@ class Place {
 
   public city: string;
   public country: string;
+  public urlPhoto: string;
 
   constructor(
     city?: string,
-    country?: string
+    country?: string,
+    urlPhoto?: string
   ){
     this.city = city;
     this.country = country;
+    this.urlPhoto = urlPhoto;
   }
+
+  public isEmpty() : boolean{
+    return !(this.city && this.country && this.urlPhoto);
+  }
+
 }
 
 export default Place;
