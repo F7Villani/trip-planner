ea7f0778fa209af04e1092daacd79a4838ad7f46
Felipe Villani <felipevillani.2000@hotmail.com>
2024-05-12 19:48:42 -0300

    feat lang pt

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="pt" className={inter.className}>
       <head>
         <link rel="shortcut icon" href="/favicon.ico" />
       </head>
