40f0119566778588a530832f72a6337e472499c5
Felipe Villani <felipevillani.2000@hotmail.com>
2024-05-16 21:05:59 -0300

    lint

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -3,11 +3,9 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="waves">
-
       <img src="./waves/wave.svg"></img>
       <img src="./waves/wave2.svg"></img>
       <img src="./waves/wave3.svg"></img>
-
     </footer>
   );
 }
