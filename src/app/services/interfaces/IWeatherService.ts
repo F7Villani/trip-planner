cfc236b18b9dc5a3f8873424cc6acbf621fa26a0
Felipe Villani <felipevillani.2000@hotmail.com>
2024-05-20 20:14:45 -0300

    feat: criando service

---
+++ src/app/services/interfaces/IWeatherService.ts
@@ -0,0 +1,7 @@
+import Weather from "../../models/Weather";
+
+interface IWeatherService {
+
+  getHistoricalWeatherByGeo(latitude: number, longitude: number, startDate: Date, endDate: Date, yearsAgo: number) : Promise<Weather>;
+
+}
