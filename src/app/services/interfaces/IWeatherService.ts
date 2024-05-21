6568379e6922459027f7abf1ba914ab8ebc126ec
Felipe Villani <felipevillani.2000@hotmail.com>
2024-05-20 22:42:08 -0300

    feat: retornando umma lista e expondo metodo

--- src/app/services/interfaces/IWeatherService.ts
+++ src/app/services/interfaces/IWeatherService.ts
@@ -2,6 +2,8 @@ import Weather from "../../models/Weather";
 
 interface IWeatherService {
 
-  getHistoricalWeatherByGeo(latitude: number, longitude: number, startDate: Date, endDate: Date, yearsAgo: number) : Promise<Weather>;
+  getHistoricalWeatherByGeo(latitude: number, longitude: number, startDate: Date, endDate: Date, yearsAgo: number) : Promise<Weather[]>;
 
 }
+
+export default IWeatherService;
