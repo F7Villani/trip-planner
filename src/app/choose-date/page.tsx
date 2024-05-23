82a9cf776ed5a589d0ccb80f731df2c640d806ca
Felipe Villani <felipevillani.2000@hotmail.com>
2024-05-22 22:54:31 -0300

    feat: chamadno api sempre que a data eh atualizada

--- src/app/choose-date/page.tsx
+++ src/app/choose-date/page.tsx
@@ -1,22 +1,44 @@
 "use client"
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import Loader from '../components/Loader/Loader';
 import "./ChooseDate.css";
 import dayjs from "dayjs";
+import IWeatherService from "../services/interfaces/IWeatherService";
+import OpenMeteoService from "../services/OpenMeteoService";
+import WeatherChart from "../components/WeatherChart/WeatherChart";
+
 
 function ChooseDate() {
 
   const [departureDay, setDepartureDay] = useState(dayjs());
-  const [leaveDay, setLeaveDay] = useState(departureDay);
+  const [leaveDay, setLeaveDay] = useState(departureDay.add(1, 'day'));
+  const [weathers, setWeathers] = useState([]);
 
-  function onChangeDepartureDay(newDate: dayjs.Dayjs){
-    setDepartureDay(newDate); 
-    if(newDate.isAfter(leaveDay)){
+  const weatherService: IWeatherService = new OpenMeteoService()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let weathers = await weatherService.getHistoricalWeatherByPeriodAndYears(52.52, 13.41, departureDay.toDate(), leaveDay.toDate(), 3);
+      setWeathers(weathers);
+    };
+
+    fetchData();
+
+  }, [leaveDay]);
+
+  function onChangeDepartureDay(newDate: dayjs.Dayjs) {
+    setDepartureDay(newDate);
+    if (newDate.isAfter(leaveDay)) {
       setLeaveDay(newDate.add(1, "day"));
+    }
+  }
+
+  function onChangeLeaveDay(newDate: dayjs.Dayjs) {
+    if (newDate.isAfter(departureDay)) {
+      setLeaveDay(newDate);
     }
   }
 
@@ -27,14 +49,16 @@ function ChooseDate() {
           <div className="calendars">
             <div className="calendar">
               <h2>Dia da Ida</h2>
-              <DateCalendar disablePast={true} value={departureDay} onChange={onChangeDepartureDay}/>
+              <DateCalendar disablePast={true} value={departureDay} onChange={onChangeDepartureDay} />
             </div>
             <div className="calendar">
               <h2>Dia da Volta</h2>
-              <DateCalendar disablePast={true} value={leaveDay} onChange={(newDate) => setLeaveDay(newDate)}/>
+              <DateCalendar disablePast={true} value={leaveDay} onChange={onChangeLeaveDay} />
             </div>
           </div>
-          <Loader />
+          <div>
+            <WeatherChart weathers={weathers[0]} />
+          </div>
         </div>
       </LocalizationProvider>
     </div>
